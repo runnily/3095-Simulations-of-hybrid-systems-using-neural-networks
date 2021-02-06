9066a59b38bdae9b69400810f812a8e707bffe52
runnily <32201807+runnily@users.noreply.github.com>
2021-02-06 22:28:00 +0000

    made plots and updated requirements list

---
+++ plots.py
@@ -0,0 +1,15 @@
+import seaborn as sns
+import matplotlib
+import pandas as pd
+
+sns.set_theme()
+df = pd.read_csv('data/heating.csv', usecols=[0,1,3], nrows=11)
+
+sns.relplot(
+    data=df, kind="line",
+    x="time", y="temp",
+    palette='rocket', 
+    #hue='state', style="state", markers=True
+)
+
+matplotlib.pyplot.show() 
