230db525eff809a88e511722d9dacf3ae8e4f804
runnily <32201807+runnily@users.noreply.github.com>
2021-04-23 21:14:16 +0100

    added new files

--- prototype.py
+++ prototype.py
@@ -192,51 +192,61 @@ def predicting_biological_model():
         'x8' : tensor_flatten(preds[:,7]),
         'x9' : tensor_flatten(preds[:,8]),
          }, ["time", "initial_x1", "initial_x2", "initial_x3", "initial_x4", "initial_x5", "initial_x6", "initial_x7", "initial_x8", "initial_x9", "x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9"])
-
+         
 def predicting_bouncing_ball():
     """
         predicting_bouncing_ball:
             This will be used uses the neural network to simulate the dynamics of a bouncing ball
     """
-
-    """
-        predicting_van_der_pol:
-            This will be used uses the neural network to simulate the dynamics of the van
-            der pol method
-    """
     filename = "data/train/bouncing.csv"
-    inputs = inputs_to_tensor(filename, [2,1,0])
+
+    inputs = inputs_to_tensor(filename, [0,1,2,4])
     targets = inputs_to_tensor(filename, [3, 4])
     
-    model = NN.CustomeModel(num_inputs = 3, num_targets = 2, learning_rate = 0.0001, layer_list = [3,128,512,2])
-    preds, _ = predictions(num_inputs=3, num_classes=2, learning_rate=0.0001, batch_size=8, num_epochs=15, inputs=inputs, targets=targets, train=True, path= MAIN_PATH + "bouncing.pth",model = model)
+    preds, _ = predictions(num_inputs = 4, num_classes=2, learning_rate=0.0001, batch_size=32, num_epochs=20, inputs=inputs, targets=targets, train=True, path= MAIN_PATH + "bouncing.pth")
     
-    time = pd.read_csv(filename, usecols=[2])
-    init_p = pd.read_csv(filename, usecols=[0])
-    init_v = pd.read_csv(filename, usecols=[1])
+    time = pd.read_csv(filename, usecols=[0])
+    init_p = pd.read_csv(filename, usecols=[1])
+    init_v = pd.read_csv(filename, usecols=[2])
    
     #save("data/preds/train/bouncing.csv", {'time' : df_flatten(time), 'initial_position' : df_flatten(init_p), 'initial_velocity' : df_flatten(init_v), 'position' : tensor_flatten(preds[:,0])}, ["time", "initial_position", "initial_velocity", "position", ])
 
     save("data/preds/train/bouncing.csv", {'time' : df_flatten(time), 'initial_position' : df_flatten(init_p), 'initial_velocity' : df_flatten(init_v), 'position' : tensor_flatten(preds[:,0]), 'velocity': tensor_flatten(preds[:,1])}, ["time", "initial_position", "initial_velocity", "position", "velocity"])
 
-def predicting_thermostat():
+def predicting_water_tank1():
     """
-        predicting_hermostat:
-            This will be used uses the neural network to simulate the dynamics of a thermostat
+        predicting_water_tank1:
+            This will be used uses the neural network to simulate the dynamics of a water tank
     """
-    filename = "data/train/thermostat3.csv"
-    inputs = inputs_to_tensor(filename, [0,1])
-    targets = inputs_to_tensor(filename, [2])
+    filename = "data/train/water_tank5.csv"
 
+    inputs = inputs_to_tensor(filename, [0,1,2,3,4,5])
+    targets = inputs_to_tensor(filename, [6,7,8,9,10])
+    
+    preds, _ = predictions(num_inputs = 6, num_classes=5, learning_rate=0.00001, batch_size=8, num_epochs=50, inputs=inputs, targets=targets, train=True, path= MAIN_PATH + "water_tank.pth")
+    
+    time = inputs[:,0]
+    init_x1 = inputs[:,1]
+    init_x2 = inputs[:,2]
+    init_x3 = inputs[:,3]
+    init_x4 = inputs[:,4]
+    init_x5 = inputs[:,5]
 
-    preds, _ = predictions(num_inputs=2, num_classes=1, learning_rate=0.00001, batch_size=1024, num_epochs=10, inputs=inputs, targets=targets, train=True, path= MAIN_PATH + "thermostat.pth")
-    time = pd.read_csv(filename, usecols=[0])
-    init_temp = pd.read_csv(filename, usecols=[1])
-    state = pd.read_csv(filename, usecols=[3])
+    #time = pd.read_csv(filename, usecols=[0])
+    #init_x1 = pd.read_csv(filename, usecols=[1])
+    #init_x2 = pd.read_csv(filename, usecols=[2])
    
-    save("data/preds/train/thermostat.csv", {'state' : df_flatten(state),'time' : df_flatten(time), 'initial_temp' : df_flatten(init_temp), 'temp' : tensor_flatten(preds)}, ["time", "initial_temp", "temp", "state"])
+    #save("data/preds/train/bouncing.csv", {'time' : df_flatten(time), 'initial_position' : df_flatten(init_p), 'initial_velocity' : df_flatten(init_v), 'position' : tensor_flatten(preds[:,0])}, ["time", "initial_position", "initial_velocity", "position", ])
+    save("data/preds/train/water_tank.csv", {'time' : tensor_flatten(time), 'initial_x1' : tensor_flatten(init_x1), 
+        'initial_x2' : tensor_flatten(init_x2), 'initial_x3' : tensor_flatten(init_x3), 
+        'initial_x4' : tensor_flatten(init_x4), 'initial_x5' : tensor_flatten(init_x5), 
+        'x1': tensor_flatten(preds[:,0]), 'x2': tensor_flatten(preds[:,1]),
+        'x3': tensor_flatten(preds[:,0]), 'x4': tensor_flatten(preds[:,0]),
+        'x5': tensor_flatten(preds[:,0]),
+        }, 
+        ["time", "initial_x1", "initial_x2", "initial_x3", "initial_x4", "initial_x5","x1", "x2", "x3", "x4", "x5"])
 
 
 if __name__== "__main__":
  print("... calculating predictions ...")
- predicting_biological_model()
+ predicting_rod_reactor()
